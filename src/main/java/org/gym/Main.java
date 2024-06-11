ac42fc415e4924df95cd06482cc77d5761569c15
Luis fernando Antunesemail <luisantunes0914@gmail.com>
2024-06-11 00:08:59 -0300

    add the rest of the Student DAO implementations

--- src/main/java/org/gym/Main.java
+++ src/main/java/org/gym/Main.java
@@ -6,23 +6,34 @@ import org.gym.model.Student;
 import org.gym.util.DB;
 
 import java.sql.Connection;
+import java.time.LocalDate;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         StudentDao student = DaoFactory.createStudentDao();
+        LocalDate birthday = LocalDate.of(1990, 1, 1);
 
-        System.out.println("Buscando Estudante por id: ");
+        System.out.println("\nBuscando Estudante por id: ");
         Student e1 = student.getStudentById(1);
         System.out.println(e1);
-        System.out.println("Buscando Estudante por cpf: ");
+
+        System.out.println("\nBuscando Estudante por cpf: ");
         Student e2 = student.getStudentByCpf("021");
         System.out.println(e2);
 
-        System.out.println("Buscando Todos os estudantes: ");
+        System.out.println("\nBuscando Todos os estudantes: ");
         List<Student> students = student.getAllStudents();
         for (Student s : students) {
             System.out.println(s);
         }
+
+        /*
+        System.out.println("\nCriando usuário");
+        Student e3 = new Student(null, "423423", "LUISAO", birthday );
+        student.addStudent(e3);
+         */
+
+
     }
 }
--- src/main/java/org/gym/dao/StudentDaoJDBC.java
+++ src/main/java/org/gym/dao/StudentDaoJDBC.java
@@ -4,10 +4,7 @@ import org.gym.model.Student;
 import org.gym.util.DB;
 import org.gym.util.DbException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,17 +19,76 @@ public class StudentDaoJDBC implements StudentDao {
 
     @Override
     public void addStudent(Student student) {
+        PreparedStatement st = null;
 
+        try {
+            st = conn.prepareStatement(
+                    "INSERT INTO students (cpf, name, birth_date) VALUES (?, ?, ?)" ,
+                    Statement.RETURN_GENERATED_KEYS
+            );
+
+            st.setString(1, student.getCpf());
+            st.setString(2, student.getName());
+            st.setDate(3, Date.valueOf(student.getBirthDate()));
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                if (rs.next()) {
+                    int id = rs.getInt(1);
+                    student.setId(id);
+                    DB.closeResultSet(rs);
+                    return;
+                }
+            }
+            throw new DbException("Unexpected error! No rows affected.");
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
     public void updateStudent(Student student) {
+        PreparedStatement st = null;
 
+        try {
+            st = conn.prepareStatement(
+                    "UPDATE students SET cpf = ?, name = ?, birth_date = ? WHERE id = ?"
+            );
+            st.setString(1, student.getCpf());
+            st.setString(2, student.getName());
+            st.setDate(3, Date.valueOf(student.getBirthDate()));
+            st.setInt(4, student.getId());
+
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
     public void deleteStudentById(int id) {
+        PreparedStatement st = null;
 
+        try {
+            st = conn.prepareStatement(
+                    "DELETE FROM students WHERE id = ?"
+            );
+            st.setInt(1, id);
+
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
